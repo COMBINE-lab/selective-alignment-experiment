fe07e57aed0ce1c482ca819c5de8c0c7364defc5
mohsenzakeri <mohsen.zakery@gmail.com>
2018-01-12 06:57:59 -0500

    rename script

--- quant_scripts/script.eval.sh
+++ quant_scripts/script.eval.sh
@@ -34,26 +34,26 @@ truthIndex="transcript_id"
 truthCount="count"
 
 #kallisto
-cmd="python script_quant_anal.py kallisto ${sample}/${kallistoResults}/abundance.tsv target_id est_counts ${sample}/${truth} ${truthIndex} ${truthCount}"
+cmd="python AnalyzeQuantification.py kallisto ${sample}/${kallistoResults}/abundance.tsv target_id est_counts ${sample}/${truth} ${truthIndex} ${truthCount}"
 eval $cmd
 
 #hera1.2
 eval "sed -i 's/\#//g' ${sample}/${heraResults}/abundance.tsv" 
 eval "sed -i 's/\:ENSG[0-9]*//g' ${sample}/${heraResults}/abundance.tsv"
-cmd="python script_quant_anal.py hera1.2 ${sample}/${heraResults}/abundance.tsv target_id est_counts ${sample}/${truth} ${truthIndex} ${truthCount}"
+cmd="python AnalyzeQuantification.py hera1.2 ${sample}/${heraResults}/abundance.tsv target_id est_counts ${sample}/${truth} ${truthIndex} ${truthCount}"
 eval $cmd
 
 #sla
-cmd="python script_quant_anal.py selective-alignment ${sample}/${slaResults}/quant.sf  Name NumReads ${sample}/${truth} ${truthIndex} ${truthCount}"
+cmd="python AnalyzeQuantification.py selective-alignment ${sample}/${slaResults}/quant.sf  Name NumReads ${sample}/${truth} ${truthIndex} ${truthCount}"
 eval $cmd
 
 
 #star
-cmd="python script_quant_anal.py star ${sample}/${starQuantResults}/quant.sf  Name NumReads ${sample}/${truth} ${truthIndex} ${truthCount}"
+cmd="python AnalyzeQuantification.py star ${sample}/${starQuantResults}/quant.sf  Name NumReads ${sample}/${truth} ${truthIndex} ${truthCount}"
 eval $cmd
 
 
 
 #bowtie2
-cmd="python script_quant_anal.py bowtie2 ${sample}/${bowtie2QuantResults}/quant.sf  Name NumReads ${sample}/${truth} ${truthIndex} ${truthCount}"
+cmd="python AnalyzeQuantification.py bowtie2 ${sample}/${bowtie2QuantResults}/quant.sf  Name NumReads ${sample}/${truth} ${truthIndex} ${truthCount}"
 eval $cmd
